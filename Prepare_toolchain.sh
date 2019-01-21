eb3c7caf0bb9257f6f834066af0f48107066fbea
khvalera <khvalera@ukr.net>
2019-01-21 23:43:01 +0200

    Archlinux support added

--- Prepare_toolchain.sh
+++ Prepare_toolchain.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 
 set -e
-
-if ! hash apt-get 2>/dev/null; then
-	whiptail --title "Orangepi Build System" --msgbox "This scripts requires a Debian based distrbution. If you not use Debian/Ubunut, pls install:[ bsdtar mtools u-boot-tools pv bc sunxi-tools gcc automake make qemu dosfstools ]"
-	exit 1
+if [[ $EUID -ne 0 ]]; then
+   whiptail --title "Orangepi Build System" --msgbox "Need to run as root" 8 30 0
+   exit 1
 fi
 
-apt-get -y --no-install-recommends --fix-missing install \
-	bsdtar mtools u-boot-tools pv bc \
-	gcc automake make \
-	lib32z1 lib32z1-dev qemu-user-static \
-	dosfstools libncurses5-dev lib32stdc++-5-dev debootstrap
+if hash apt-get 2>/dev/null; then
+   apt-get -y --no-install-recommends --fix-missing install \
+            bsdtar mtools u-boot-tools pv bc \
+            gcc automake make \
+            lib32z1 lib32z1-dev qemu-user-static \
+            dosfstools libncurses5-dev lib32stdc++-5-dev debootstrap
+elif hash pacman 2>/dev/null; then
+     pacman -Sy mtools uboot-tools pv bc \
+            qemu-user-static \
+            dosfstools libstdc++5 debootstrap --noconfirm
+else
+  whiptail --title "Orangepi Build System" --msgbox "This script requires a Debian or Archlinux based distribution. If you are using another distribution, install:[ bsdtar mtools u-boot-tools pv bc sunxi-tools gcc automake make qemu dosfstools ]" 10 50 0
+  exit 1
+fi
 
 # Prepare toolchains
 ROOT=`cd .. && pwd`
